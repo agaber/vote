4b0c97b68b069543f180416054ff07ea7bcf803c
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-13 23:58:35 -0500

    Explicitly set OK 200 status in healthz servlet.

--- service/src/main/java/dev/agaber/vote/service/server/HealthzServlet.java
+++ service/src/main/java/dev/agaber/vote/service/server/HealthzServlet.java
@@ -3,10 +3,11 @@ package dev.agaber.vote.service.server;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 
-/** Returns a 200 OK status if the server is still running. That's it. */
+/** Returns a 200 OK status if the server is running. That's it. */
 public class HealthzServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
@@ -14,5 +15,6 @@ public class HealthzServlet extends HttpServlet {
       printWriter.println("<!DOCTYPE html>");
       printWriter.println("<html><body>ok</body></html>");
     }
+    response.setStatus(HttpStatus.OK.value());
   }
 }
