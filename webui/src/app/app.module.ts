ed927ec32832d43fbaf53400f912b3813024f816
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-20 08:36:09 -0500

    Update tsconfig to allow imports with absolute paths.

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from '//app/app-routing.module';
+import { AppComponent } from '//app/app.component';
+import { MaterialModule } from '//app/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
