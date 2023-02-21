2849ad18eb5dc21c14f609b49ac9001c4b29e87b
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-21 15:59:02 -0500

    Implement election.service.ts and tests.

--- webui/src/app/services/election.service.spec.ts
+++ webui/src/app/services/election.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { Election } from '@/app/model/election';
 import { ElectionService } from '@/app/services/election.service';
 import { environment as env } from '@/environments/environment';
+import { Vote } from '@/app/model/vote';
 
 describe('ElectionService', () => {
   let httpMock: HttpTestingController;
@@ -40,5 +41,55 @@ describe('ElectionService', () => {
     const req = httpMock.expectOne(`${env.apiUrl}/elections`);
     expect(req.request.method).toEqual('POST');
     req.flush(expectedElection);
+  });
+
+  it('should get an election by ID', () => {
+    const election: Election = {
+      id: '12345',
+      question: 'Will this test pass?',
+      options: ['Yes', 'Yep', 'naturally'],
+    };
+
+    service.getById(election.id!).subscribe(result => {
+      expect(result).toEqual(election);
+    });
+
+    const req = httpMock.expectOne(`${env.apiUrl}/elections/${election.id}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(election);
+  });
+
+  it('should vote in an election', () => {
+    const election: Election = {
+      id: '12345',
+      question: 'Will this test pass?',
+      options: ['Yes', 'Yep', 'naturally'],
+    };
+
+    const vote: Vote = {
+      id: '54321',
+      electionId: election.id,
+      choices: ['Yes', 'Yep'],
+    }
+
+    service.vote(election.id!, vote.choices).subscribe(result => {
+      expect(result).toEqual(vote);
+    });
+
+    const req = httpMock.expectOne(`${env.apiUrl}/elections/${election.id}:vote`);
+    expect(req.request.method).toEqual('POST');
+    req.flush(vote);
+  });
+
+  it('should tally the results of an election', () => {
+    const electionId = '12345';
+
+    service.tally(electionId).subscribe(result => {
+      expect(result).toEqual('Yes');
+    });
+
+    const req = httpMock.expectOne(`${env.apiUrl}/elections/${electionId}:tally`);
+    expect(req.request.method).toEqual('POST');
+    req.flush('Yes');
   });
 });
--- webui/src/app/services/election.service.ts
+++ webui/src/app/services/election.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { environment as env } from '@/environments/environment';
 import { Election } from '@/app/model/election';
+import { environment as env } from '@/environments/environment';
 import { Observable } from 'rxjs';
+import { Vote } from '@/app/model/vote';
 
 const HTTP_OPTONS = {
   headers: new HttpHeaders({
@@ -19,5 +20,20 @@ export class ElectionService {
   create(election: Election): Observable<Election> {
     const url = `${env.apiUrl}/elections`
     return this.http.post<Election>(url, election, HTTP_OPTONS);
+  }
+
+  getById(electionId: string): Observable<Election> {
+    const url = `${env.apiUrl}/elections/${electionId}`;
+    return this.http.get<Election>(url);
+  }
+
+  vote(electionId: string, choices: string[]): Observable<Vote> {
+    const url = `${env.apiUrl}/elections/${electionId}:vote`;
+    return this.http.post<Vote>(url, undefined, HTTP_OPTONS);
+  }
+
+  tally(electionId: string): Observable<string> {
+    const url = `${env.apiUrl}/elections/${electionId}:tally`;
+    return this.http.post<string>(url, undefined, HTTP_OPTONS);
   }
 }
