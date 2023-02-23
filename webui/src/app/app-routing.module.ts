70965e6b8a735f1db53cbe4ee8d4a1053cabb860
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-23 01:15:01 -0500

    CSS styling and routing for the create election component.

--- webui/src/app/app-routing.module.ts
+++ webui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateElectionComponent } from './components/create-election/create-election.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CreateElectionComponent },
+  { path: 'create', component: CreateElectionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
