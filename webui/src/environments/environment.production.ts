4eb90b1fac95d5e5079dde36acb074ff4b0d9efa
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-28 22:18:01 -0500

    Fix share URL and add a few more demo elections.

--- webui/src/environments/environment.production.ts
+++ webui/src/environments/environment.production.ts
@@ -1,7 +1,9 @@
 export const environment = {
   apiUrl: 'https://apis.agaber.dev/vote/api/v1',
-  uiUrl: 'https://agaber.dev',
+  uiUrl: 'https://vote.agaber.dev',
   demoElectionIds: [
     '63fe7ba7a0495e1d41fd1641', // Animal Kingdom
+    '63fec194caab6418a3df6459', // Favorite Programming Language
+    '63fec28db19c0f2f69516a1e', // Who will win the 2023 NBA Finals
   ],
 };
