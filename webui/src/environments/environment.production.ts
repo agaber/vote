53da8dcb53428a1e02a00432447753979a6d3d9f
Abrahim Gaber <abrahimagaber@gmail.com>
2023-02-28 19:22:52 -0500

    Add prod demo election ID.

--- webui/src/environments/environment.production.ts
+++ webui/src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
   apiUrl: 'https://apis.agaber.dev/vote/api/v1',
   uiUrl: 'https://agaber.dev',
   demoElectionIds: [
-
+    '63fe7ba7a0495e1d41fd1641', // Animal Kingdom
   ],
 };
